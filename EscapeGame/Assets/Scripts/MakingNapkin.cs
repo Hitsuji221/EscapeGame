8b13a588c0f8bacb19a36f3328e50143d4b5966d
Hitsuji221 <67304907+Hitsuji221@users.noreply.github.com>
2020-10-09 00:04:28 +0900

    ナプキンゲーム完成版

--- EscapeGame/Assets/Scripts/MakingNapkin.cs
+++ EscapeGame/Assets/Scripts/MakingNapkin.cs
@@ -10,14 +10,27 @@ public class MakingNapkin : MonoBehaviour
     public static MakingNapkin instance;
     [SerializeField] GameObject answerisno;
     [SerializeField] GameObject answerpanel;
+    public GameObject kizi;
+    public GameObject pori;
+    public GameObject cotton;
     public GameObject kizi2;
     public GameObject pori2;
     public GameObject cotton2;
+    public GameObject mkizi1;
+    public GameObject mkizi2;
+    public GameObject mkizi3;
+    public GameObject message1;
+    public GameObject message2;
+    public GameObject toolBox;
+
     string currentPanelStr = "Image1";
 
     void Start(){
         answerisno.SetActive(false);
         answerpanel.SetActive(false);
+        toolBox.SetActive(true);
+        message1.SetActive(false);
+        message2.SetActive(false);
     }
     public void OnClickStart(){
         this.transform.localPosition=new Vector2(0,0);
@@ -25,21 +38,35 @@ public class MakingNapkin : MonoBehaviour
     }
     public void OnClickPen(){
         if(currentPanelStr == "Image1"){ //Panel0のstomacgirlを表示
-            this.transform.localPosition=new Vector2(-1200,0);
-            currentPanelStr = "Image2";
+            this.transform.localPosition=new Vector2(0,1000);
+            currentPanelStr = "Image11";
+            OnClickCorrectOne();
+        }else if(currentPanelStr == "Image11" && (kizi.activeSelf == false || pori.activeSelf == false || cotton.activeSelf == false)){
+            OnClickCorrectOne();
+        }else if(currentPanelStr == "Image6"){
+            this.transform.localPosition=new Vector2(-4800,1000);
+            currentPanelStr = "Image61";
             OnClickCorrectOne();
         }else if(currentPanelStr == "Image4"||currentPanelStr == "Image0"){
+            OnClickCorrectOne();   
+        }else if(currentPanelStr == "Image61" && (mkizi1.activeSelf == false)){
             OnClickCorrectOne();
         }else{
             OnClickOtherOne();
         }
     }
      public void OnClickHasami(){
-        if(currentPanelStr == "Image2"){ //Panel0のstomacgirlを表示
+        if(currentPanelStr == "Image11"&& (kizi.activeSelf == true && pori.activeSelf == true && cotton.activeSelf == true)){ //Panel0のstomacgirlを表示
             this.transform.localPosition=new Vector2(-1200,1000);
             currentPanelStr = "Image21";
             OnClickCorrectOne();
         }else if(currentPanelStr == "Image21" && (kizi2.activeSelf == false || pori2.activeSelf == false || cotton2.activeSelf == false)){
+            OnClickCorrectOne();
+        }else if(currentPanelStr == "Image61"&& (mkizi1.activeSelf == true)){
+            this.transform.localPosition=new Vector2(-4800,2000);
+            currentPanelStr = "Image62";
+            OnClickCorrectOne();
+        }else if(currentPanelStr == "Image62" && (mkizi2.activeSelf == false)){
             OnClickCorrectOne();
         }else if(currentPanelStr == "Image4"||currentPanelStr == "Image0"){
             OnClickCorrectOne();
@@ -51,6 +78,12 @@ public class MakingNapkin : MonoBehaviour
         if(currentPanelStr == "Image21" && kizi2.activeSelf == true && pori2.activeSelf == true && cotton2.activeSelf == true ){ //Panel0のstomacgirlを表示
             this.transform.localPosition=new Vector2(-2400,1000);
             currentPanelStr = "Image41";
+            OnClickCorrectOne();
+        }else if(currentPanelStr == "Image62"&& (mkizi2.activeSelf == true)){
+            this.transform.localPosition=new Vector2(-4800,3000);
+            currentPanelStr = "Image63";
+            OnClickCorrectOne();
+        }else if(currentPanelStr == "Image63" && (mkizi3.activeSelf == false)){
             OnClickCorrectOne();
         }else if(currentPanelStr == "Image41"||currentPanelStr == "Image0" ||currentPanelStr == "Image4"){
             OnClickCorrectOne();
@@ -73,6 +106,38 @@ public class MakingNapkin : MonoBehaviour
     public void OnClickPanel(){
         this.transform.localPosition=new Vector2(-2400,0);
         currentPanelStr = "Image41";
+        OnClickCorrectOne();
+    }
+    public void OnClickNapkin(){
+        this.transform.localPosition=new Vector2(-3600,0);
+        currentPanelStr = "Image5";
+        toolBox.SetActive(false);
+        message1.SetActive(true);
+        OnClickCorrectOne();
+    }
+    public void OnClickNextGame(){
+        this.transform.localPosition=new Vector2(-3600,1000);
+        currentPanelStr = "Image51";
+        toolBox.SetActive(false);
+        OnClickCorrectOne();
+    }
+    public void OnClickGameStart2(){
+        this.transform.localPosition=new Vector2(-4800,0);
+        currentPanelStr = "Image6";
+        toolBox.SetActive(true);
+        OnClickCorrectOne();
+    }
+    public void OnClickMawashi(){
+        this.transform.localPosition=new Vector2(-6000,0);
+        currentPanelStr = "Image7";
+        toolBox.SetActive(false);
+        message2.SetActive(true);
+        OnClickCorrectOne();
+    }
+    public void OnClickGattai(){
+        this.transform.localPosition=new Vector2(-6000,1000);
+        currentPanelStr = "Image71";
+        toolBox.SetActive(false);
         OnClickCorrectOne();
     }
 }
